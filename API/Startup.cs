268182575cd65c40a7532370baf48d4d9fc64bf2
Κορακάκης Μιχαήλ (66005) <MKorakakis@day.int.gr>
2020-03-27 14:46:17 +0200

    Added React with semantic ui

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,11 @@ namespace API
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
-
+      services.AddCors(opt => {
+        opt.AddPolicy("CorsPolicy", policy => {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
       services.AddControllers();
     }
 
@@ -44,6 +48,7 @@ namespace API
       }
 
       // app.UseHttpsRedirection();
+      app.UseCors("CorsPolicy");
 
       app.UseRouting();
 
