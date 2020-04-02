a345a726b49e41b05ab2eb76d810958b171dfe10
Mike Korakakis <mike.korakakis@gmail.com>
2020-04-02 22:47:48 +0300

    added comments

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,98 +22,116 @@ using Persistence;
 using AutoMapper;
 using Infrastructure.Photos;
 using Application;
+using System.Threading.Tasks;
+using API.SignalR;
 
 namespace API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseLazyLoadingProxies();
-        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
+        public Startup(IConfiguration configuration)
         {
-          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-        });
-      });
+            Configuration = configuration;
+        }
 
-      services.AddMediatR(typeof(List.Handler).Assembly);
+        public IConfiguration Configuration { get; }
 
-      services.AddAutoMapper(typeof(List.Handler));
-
-
-      services.AddControllers(opt =>
-      {
-        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-        opt.Filters.Add(new AuthorizeFilter(policy));
-      }).AddFluentValidation(cfg => { cfg.RegisterValidatorsFromAssemblyContaining<Create>(); });
-
-      var builder = services.AddIdentityCore<AppUser>();
-      var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-      identityBuilder.AddEntityFrameworkStores<DataContext>();
-      identityBuilder.AddSignInManager<SignInManager<AppUser>>();
-
-      services.AddAuthorization(opt =>
-      {
-        opt.AddPolicy("IsActivityHost", policy =>
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-          policy.Requirements.Add(new IsHostRequirement());
-        });
-      });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseLazyLoadingProxies();
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+          {
+              policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowAnyHeader();
+          });
+            });
 
-      services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+            services.AddMediatR(typeof(List.Handler).Assembly);
 
-      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
-      {
-        opt.TokenValidationParameters = new TokenValidationParameters
+            services.AddAutoMapper(typeof(List.Handler));
+
+            services.AddSignalR();
+
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            }).AddFluentValidation(cfg => { cfg.RegisterValidatorsFromAssemblyContaining<Create>(); });
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("IsActivityHost", policy =>
+          {
+              policy.Requirements.Add(new IsHostRequirement());
+          });
+            });
+
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = key,
-          ValidateAudience = false,
-          ValidateIssuer = false
-        };
-      });
-      services.AddScoped<IJwtGenerator, JwtGenerator>();
-      services.AddScoped<IUserAccessor, UserAccessor>();
-      services.AddScoped<IPhotoAccessor, PhotoAccessor>();
-      services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            if (env.IsDevelopment())
+            {
+                // app.UseDeveloperExceptionPage();
+            }
 
+            // app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      app.UseMiddleware<ErrorHandlingMiddleware>();
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-      }
-
-      // app.UseHttpsRedirection();
-      app.UseRouting();
-      app.UseCors("CorsPolicy");
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -6,205 +6,262 @@ import { history } from "../../index";
 import { toast } from "react-toastify";
 import { RootStore } from "./rootStore";
 import { setActivityProps, createAttendee } from "../common/util/util";
+import {
+	HubConnection,
+	HubConnectionBuilder,
+	LogLevel
+} from "@microsoft/signalr";
+import { id } from "date-fns/locale";
 
 export default class ActivityStore {
-    rootStore: RootStore;
+	rootStore: RootStore;
 
-    constructor(rootStore: RootStore) {
-        this.rootStore = rootStore;
-    }
+	constructor(rootStore: RootStore) {
+		this.rootStore = rootStore;
+	}
 
-    @observable activityRegistry = new Map();
-    @observable activity: IActivity | null = null;
-    @observable loadingInitial = false;
-    @observable submitting = false;
-    @observable target = "";
-    @observable loading = false;
+	@observable activityRegistry = new Map();
+	@observable activity: IActivity | null = null;
+	@observable loadingInitial = false;
+	@observable submitting = false;
+	@observable target = "";
+	@observable loading = false;
+	@observable.ref hubConnection: HubConnection | null = null;
 
-    @computed get activitiesByDate() {
-        return this.groupActivitiesByDate(
-            Array.from(this.activityRegistry.values())
-        );
-    }
-    groupActivitiesByDate(activities: IActivity[]) {
-        const sortedActivities = activities.sort(
-            (a, b) => a.date.getTime() - b.date.getTime()
-        );
-        return Object.entries(
-            sortedActivities.reduce((activities, activity) => {
-                const date = activity.date.toISOString().split("T")[0];
-                activities[date] = activities[date]
-                    ? [...activities[date], activity]
-                    : [activity];
-                return activities;
-            }, {} as { [key: string]: IActivity[] })
-        );
-    }
+	@action createHubConnection = (activityId: string) => {
+		this.hubConnection = new HubConnectionBuilder()
+			.withUrl("http://localhost:5000/chat", {
+				accessTokenFactory: () => this.rootStore.commonStore.token!
+			})
+			.configureLogging(LogLevel.Information)
+			.build();
 
-    @action loadActivities = async () => {
-        this.loadingInitial = true;
-        try {
-            const activities = await agent.Activities.list();
-            runInAction("loading activities", () => {
-                activities.forEach(activity => {
-                    setActivityProps(activity, this.rootStore.userStore.user!);
-                    this.activityRegistry.set(activity.id, activity);
-                });
-                this.loadingInitial = false;
-            });
-        } catch (error) {
-            runInAction("load activities error", () => {
-                this.loadingInitial = false;
-            });
-        }
-    };
+		this.hubConnection
+			.start()
+			.then(() => console.log(this.hubConnection!.state))
+			.then(() => {
+				console.log("Attempting to join room");
+				this.hubConnection!.invoke("AddToGroup", activityId);
+			})
+			.catch(error =>
+				console.log("Error establishing connection: ", error)
+			);
 
-    @action loadActivity = async (id: string) => {
-        let activity = this.getActivity(id);
-        if (activity) {
-            this.activity = activity;
-            return activity;
-        } else {
-            this.loadingInitial = true;
-            try {
-                activity = await agent.Activities.details(id);
-                runInAction("getting activity", () => {
-                    setActivityProps(activity, this.rootStore.userStore.user!);
+		this.hubConnection.on("ReceiveComment", comment => {
+			runInAction(() => this.activity!.comments.push(comment));
+			this.activity!.comments.push(comment);
+		});
 
-                    this.activity = activity;
-                    this.activityRegistry.set(activity.id, activity);
-                    this.loadingInitial = false;
-                });
-                return activity;
-            } catch (error) {
-                runInAction("get activity error", () => {
-                    this.loadingInitial = false;
-                });
-                console.log(error);
-            }
-        }
-    };
+		this.hubConnection.on("Send", message => {
+			toast.info(message);
+		});
+	};
 
-    @action clearActivity = () => {
-        this.activity = null;
-    };
+	@action stopHubConnection = () => {
+		this.hubConnection!.invoke("RemoveFromGroup", this.activity!.id)
+			.then(() => {
+				this.hubConnection!.stop();
+			})
+			.then(() => {
+				console.log("Connection stopped");
+			})
+			.catch(err => console.log(err));
+	};
 
-    getActivity = (id: string) => {
-        return this.activityRegistry.get(id);
-    };
+	@action addComment = async (values: any) => {
+		values.activityId = this.activity!.id;
+		try {
+			await this.hubConnection!.invoke("SendComment", values);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    @action createActivity = async (activity: IActivity) => {
-        this.submitting = true;
+	@computed get activitiesByDate() {
+		return this.groupActivitiesByDate(
+			Array.from(this.activityRegistry.values())
+		);
+	}
+	groupActivitiesByDate(activities: IActivity[]) {
+		const sortedActivities = activities.sort(
+			(a, b) => a.date.getTime() - b.date.getTime()
+		);
+		return Object.entries(
+			sortedActivities.reduce((activities, activity) => {
+				const date = activity.date.toISOString().split("T")[0];
+				activities[date] = activities[date]
+					? [...activities[date], activity]
+					: [activity];
+				return activities;
+			}, {} as { [key: string]: IActivity[] })
+		);
+	}
 
-        try {
-            await agent.Activities.create(activity);
-            const attendee = createAttendee(this.rootStore.userStore.user!);
-            attendee.isHost = true;
-            let attendees = [];
-            attendees.push(attendee);
-            activity.attendees = attendees;
-            activity.isHost = true;
-            runInAction("create activity", () => {
-                this.activityRegistry.set(activity.id, activity);
+	@action loadActivities = async () => {
+		this.loadingInitial = true;
+		try {
+			const activities = await agent.Activities.list();
+			runInAction("loading activities", () => {
+				activities.forEach(activity => {
+					setActivityProps(activity, this.rootStore.userStore.user!);
+					this.activityRegistry.set(activity.id, activity);
+				});
+				this.loadingInitial = false;
+			});
+		} catch (error) {
+			runInAction("load activities error", () => {
+				this.loadingInitial = false;
+			});
+		}
+	};
 
-                this.submitting = false;
-            });
-            history.push(`/activities/${activity.id}`);
-        } catch (error) {
-            runInAction("create activity error", () => {
-                this.submitting = false;
-            });
-            toast.error("Problem submittin data");
+	@action loadActivity = async (id: string) => {
+		let activity = this.getActivity(id);
+		if (activity) {
+			this.activity = activity;
+			return activity;
+		} else {
+			this.loadingInitial = true;
+			try {
+				activity = await agent.Activities.details(id);
+				runInAction("getting activity", () => {
+					setActivityProps(activity, this.rootStore.userStore.user!);
 
-            console.log(error.response);
-        }
-    };
+					this.activity = activity;
+					this.activityRegistry.set(activity.id, activity);
+					this.loadingInitial = false;
+				});
+				return activity;
+			} catch (error) {
+				runInAction("get activity error", () => {
+					this.loadingInitial = false;
+				});
+				console.log(error);
+			}
+		}
+	};
 
-    @action editActivity = async (activity: IActivity) => {
-        this.submitting = true;
+	@action clearActivity = () => {
+		this.activity = null;
+	};
 
-        try {
-            await agent.Activities.update(activity);
-            runInAction("editing an activity", () => {
-                this.activityRegistry.set(activity.id, activity);
-                this.activity = activity;
-                this.submitting = false;
-            });
-            history.push(`/activities/${activity.id}`);
-        } catch (error) {
-            runInAction("editing an activity error", () => {
-                this.submitting = false;
-            });
-            toast.error("Problem submittin data");
+	getActivity = (id: string) => {
+		return this.activityRegistry.get(id);
+	};
 
-            console.log(error.response);
-        }
-    };
+	@action createActivity = async (activity: IActivity) => {
+		this.submitting = true;
 
-    @action deleteActivity = async (
-        event: SyntheticEvent<HTMLButtonElement>,
-        id: string
-    ) => {
-        this.submitting = true;
-        this.target = event.currentTarget.name;
-        await agent.Activities.delete(id);
-        try {
-            runInAction("deleting an activity", () => {
-                this.activityRegistry.delete(id);
-                this.submitting = false;
-                this.target = "";
-            });
-        } catch (error) {
-            runInAction("deleting an activity error", () => {
-                this.submitting = false;
-                this.target = "";
-            });
-            console.log(error);
-        }
-    };
+		try {
+			await agent.Activities.create(activity);
+			const attendee = createAttendee(this.rootStore.userStore.user!);
+			attendee.isHost = true;
+			let attendees = [];
+			attendees.push(attendee);
+			activity.attendees = attendees;
+			activity.comments = [];
+			activity.isHost = true;
+			runInAction("create activity", () => {
+				this.activityRegistry.set(activity.id, activity);
 
-    @action attendActivity = async () => {
-        const attendee = createAttendee(this.rootStore.userStore.user!);
-        this.loading = true;
-        try {
-            await agent.Activities.attend(this.activity!.id);
-            runInAction(() => {
-                if (this.activity) {
-                    this.activity.attendees.push(attendee);
-                    this.activity.isGoing = true;
-                    this.activityRegistry.set(this.activity.id, this.activity);
-                    this.loading = false;
-                }
-            });
-        } catch (error) {
-            runInAction(() => {
-                this.loading = false;
-            });
-            toast.error("Problem signing up to activity");
-        }
-    };
+				this.submitting = false;
+			});
+			history.push(`/activities/${activity.id}`);
+		} catch (error) {
+			runInAction("create activity error", () => {
+				this.submitting = false;
+			});
+			toast.error("Problem submittin data");
 
-    @action cancelAttendance = async () => {
-        this.loading = true;
-        try {
-            await agent.Activities.unattend(this.activity!.id);
-            runInAction(() => {
-                if (this.activity) {
-                    this.activity.attendees = this.activity.attendees.filter(
-                        a =>
-                            a.username !==
-                            this.rootStore.userStore.user!.username
-                    );
-                    this.activity.isGoing = true;
-                    this.activityRegistry.set(this.activity.id, this.activity);
-                    this.loading = false;
-                }
-            });
-        } catch (error) {
-            runInAction(() => {
-                this.loading = false;
-            });
-            toast.error("Problem cancelling attendance");
-        }
-    };
+			console.log(error.response);
+		}
+	};
+
+	@action editActivity = async (activity: IActivity) => {
+		this.submitting = true;
+
+		try {
+			await agent.Activities.update(activity);
+			runInAction("editing an activity", () => {
+				this.activityRegistry.set(activity.id, activity);
+				this.activity = activity;
+				this.submitting = false;
+			});
+			history.push(`/activities/${activity.id}`);
+		} catch (error) {
+			runInAction("editing an activity error", () => {
+				this.submitting = false;
+			});
+			toast.error("Problem submittin data");
+
+			console.log(error.response);
+		}
+	};
+
+	@action deleteActivity = async (
+		event: SyntheticEvent<HTMLButtonElement>,
+		id: string
+	) => {
+		this.submitting = true;
+		this.target = event.currentTarget.name;
+		await agent.Activities.delete(id);
+		try {
+			runInAction("deleting an activity", () => {
+				this.activityRegistry.delete(id);
+				this.submitting = false;
+				this.target = "";
+			});
+		} catch (error) {
+			runInAction("deleting an activity error", () => {
+				this.submitting = false;
+				this.target = "";
+			});
+			console.log(error);
+		}
+	};
+
+	@action attendActivity = async () => {
+		const attendee = createAttendee(this.rootStore.userStore.user!);
+		this.loading = true;
+		try {
+			await agent.Activities.attend(this.activity!.id);
+			runInAction(() => {
+				if (this.activity) {
+					this.activity.attendees.push(attendee);
+					this.activity.isGoing = true;
+					this.activityRegistry.set(this.activity.id, this.activity);
+					this.loading = false;
+				}
+			});
+		} catch (error) {
+			runInAction(() => {
+				this.loading = false;
+			});
+			toast.error("Problem signing up to activity");
+		}
+	};
+
+	@action cancelAttendance = async () => {
+		this.loading = true;
+		try {
+			await agent.Activities.unattend(this.activity!.id);
+			runInAction(() => {
+				if (this.activity) {
+					this.activity.attendees = this.activity.attendees.filter(
+						a =>
+							a.username !==
+							this.rootStore.userStore.user!.username
+					);
+					this.activity.isGoing = true;
+					this.activityRegistry.set(this.activity.id, this.activity);
+					this.loading = false;
+				}
+			});
+		} catch (error) {
+			runInAction(() => {
+				this.loading = false;
+			});
+			toast.error("Problem cancelling attendance");
+		}
+	};
 }
